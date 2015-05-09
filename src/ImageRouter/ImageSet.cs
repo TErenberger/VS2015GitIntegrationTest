e422597a02b9a15a7fd1542893007ae22248e02b
Trey Erenberger <treye@iastate.edu>
2015-05-09 16:29:25 -0500

    added file recurse for folder

---
+++ src/ImageRouter/ImageSet.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRouter
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class ImageSet
+    {
+        private HashSet<String> images;
+
+        public ImageSet()
+        {
+            images = new HashSet<string>();
+        }
+
+        public void GatherImages(String folder)
+        {
+            try
+            {
+                foreach (String currentDir in Directory.GetDirectories(folder))
+                {
+                    foreach (String currentFile in Directory.GetFiles(currentDir))
+                    {
+                        if(currentFile.Contains(".png"))
+                        {
+                            Console.WriteLine(currentFile);
+                            images.Add(currentFile);
+
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                
+            }
+        }
+
+        private void AddImage(String url)
+        {
+            this.images.Add(url);
+        }
+
+        public Boolean HasImage(String url)
+        {
+            return this.images.Contains(url);
+        }
+    }
+}
