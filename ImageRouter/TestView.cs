c25dec7043179e753bb80f3ca091281c25f7879c
Trey Erenberger <treye@iastate.edu>
2015-05-09 17:41:49 -0500

    switch to desktop project type

---
+++ ImageRouter/TestView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageRouter
+{
+    class TestView
+    {
+        public static void Main(String[] args)
+        {
+            ImageSet images = new ImageSet();
+            images.GatherImages("D:\\");
+            Console.ReadLine();
+        }
+    }
+}
